e164ee465cf0fd2b427ea6061cff4b69e6472f46
Chris Johnston <chris@thejohnstons.net>
2020-05-02 20:46:26 -0700

    Add support for modifier keys, typing a whole string

--- src/thebutton/thebutton.ino
+++ src/thebutton/thebutton.ino
@@ -1,10 +1,22 @@
+// The button
+// Handles a single button to act as a keyboard input
+
 #include <Arduino.h>
 #include <DigiKeyboard.h>
 
-#define PIN_BUTTON 5
-#define PIN_LED 0
+#define PIN_BUTTON 0
+#define PIN_LED 1
 
+// Uncomment to send a single keystroke
 #define KEYSTROKE KEY_F // to pay respects
+
+// Uncomment to to send a keystroke + modifier
+// #define KEYSTROKE KEY_L
+// #define KEYSTROKE_MOD MOD_GUI_LEFT
+
+// Uncomment to send a string
+// #define KEY_MESSAGE "github.com/Chris-Johnston/the-button"
+
 #define DEBOUNCE_MS 50
 
 unsigned long lastDebounceTime = 0;
@@ -15,34 +27,58 @@ void setup()
 {
     pinMode(PIN_LED, OUTPUT);
     pinMode(PIN_BUTTON, INPUT);
+
+    // blink on startup
+    for (int i = 0; i < 5; i++)
+    {
+      digitalWrite(PIN_LED, HIGH);
+      delay(50);
+      digitalWrite(PIN_LED, LOW);
+      delay(50);
+    }
 }
 
 void loop()
 {
-    DigiKeyboard.update();
     int state = digitalRead(PIN_BUTTON);
 
-    if (state != lastButtonState)
+    if (state)
     {
-        lastDebounceTime = millis();
+      digitalWrite(PIN_LED, HIGH);
     }
-
-    if ((millis() - lastDebounceTime) > DEBOUNCE_MS)
+    else
     {
-        if (state != buttonState)
-        {
-            buttonState = state;
+     analogWrite(PIN_LED, abs(255 * sin((millis() - lastDebounceTime) / 1000.0))); 
+    }
+    
+    if (state != buttonState && (millis() - lastDebounceTime) > DEBOUNCE_MS)
+    {
+        buttonState = state;
+        lastDebounceTime = millis();
 
-            if (buttonState == HIGH)
-            {
-                DigiKeyboard.sendKeyPress(KEYSTROKE);
-            }
-            else
-            {
-                // release
-                DigiKeyboard.sendKeyPress(0);
-            }
-            
+        if (buttonState == HIGH)
+        {
+            digitalWrite(PIN_LED, HIGH);
+
+            #ifdef KEYSTROKE
+                #ifdef KEYSTROKE_MOD
+                    DigiKeyboard.sendKeyPress(KEYSTROKE, KEYSTROKE_MOD);
+                #else
+                    DigiKeyboard.sendKeyPress(KEYSTROKE);
+                #endif
+            #endif
+
+            #ifdef KEY_MESSAGE
+            DigiKeyboard.write(KEY_MESSAGE);
+            #endif
+        }
+        else
+        {
+            #ifdef KEYSTROKE
+            // release
+            DigiKeyboard.sendKeyPress(0);
+            #endif
         }
     }
+    DigiKeyboard.update();
 }
